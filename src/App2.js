23ba674e6665661a0f7eb62a50d8e89873f59e8f
Wavist <sommy.okafor@yahoo.com>
2020-11-09 10:53:59 -0800

    done with initial build
    Signed-off-by: Wavist <sommy.okafor@yahoo.com>

---
+++ src/App2.js
@@ -0,0 +1,61 @@
+import React, { useState } from "react";
+// import keys from "./keys";
+
+const api = {
+  key: process.env.REACT_APP_WEATHER_KEY,
+  base: process.env.REACT_APP_BASE_URL,
+};
+
+function Appp() {
+  const [query, setQuery] = useState("");
+  const [weather, setWeather] = useState({});
+  const search = (e) => {
+    if (e.key === "Enter") {
+      fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
+        .then((res) => res.json())
+        .then((result) => {
+          setQuery("");
+          setWeather(result);
+          console.log(result);
+        });
+    }
+  };
+
+  return (
+    <div
+      className="App2"
+    >
+      <main>
+        <div className="search-container">
+          <input
+            type="text"
+            placeholder="Search..."
+            className="search-bar"
+            onChange={(e) => setQuery(e.target.value)}
+            value={query}
+            onKeyPress={search}
+          />
+        </div>
+        {typeof weather.main != "undefined" ? (
+          <div>
+            <div className="location-container">
+              <div className="location">
+                {weather.name}, {weather.sys.country}
+              </div>
+            </div>
+            <div className="weather-container">
+              <div className="temperature">
+                {Math.round(weather.main.temp)}°
+              </div>
+              <div className="weather">{weather.weather[0].main}</div>
+            </div>
+          </div>
+        ) : (
+          ""
+        )}
+      </main>
+    </div>
+  );
+}
+
+export default Appp;
